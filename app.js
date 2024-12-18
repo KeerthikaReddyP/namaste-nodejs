8ede63bbf34e8c00bb47611cf4ae41f979795e5f
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-18 12:11:38 +0530

    require

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-
+require("./xyz.js");
 console.log(globalThis===global);
 
 // How do we executre xyz.js also?
 // node app.js command --> entry point is app.js file
+
+// We should include xyz.js file in app.js file
+// using require function
+// It takes a path
+
+// require function includes one module inside another module
+// app.js is one module and xyz.js is another module
