6e18c5f3575235a2e0f37cd5b6d2d7d1ba317084
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-18 12:18:34 +0530

    Can we do this? No

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 require("./xyz.js");
+require("./sum.js");
 console.log(globalThis===global);
 
-// How do we executre xyz.js also?
-// node app.js command --> entry point is app.js file
+var a=10;
+var b=20;
+calculateSum(a,b);
 
-// We should include xyz.js file in app.js file
-// using require function
-// It takes a path
-
-// require function includes one module inside another module
-// app.js is one module and xyz.js is another module
+// Error : calculateSum is not defined
