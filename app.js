5421af92d1ecd14875cdcb2c8081d86bb6f55c9a
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-18 12:09:07 +0530

    How to execute other files?

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
- console.log(globalThis);
 
- console.log(globalThis===global);
+console.log(globalThis===global);
+
+// How do we executre xyz.js also?
+// node app.js command --> entry point is app.js file
---
+++ xyz.js
@@ -0,0 +1,2 @@
+
+console.log("Very important code");
