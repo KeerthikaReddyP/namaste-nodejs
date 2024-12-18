326ae499d37eda1c6af8e143cd832679f7781e41
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-18 14:08:42 +0530

    It'll work fine even without .js extension in require

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("./xyz.js");
 
-const {x,calculateSum}=require("./sum.js");
+const {x,calculateSum}=require("./sum");
 
 console.log(globalThis===global);
 
@@ -9,3 +9,6 @@ var b=20;
 calculateSum(a,b);
 
 console.log(x);
+
+// It will work fine without .js extension too.
+// If we don't use any extension, it'll be considered .js extension.
