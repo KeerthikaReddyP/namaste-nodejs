7bfaf1f83938b622bfbfd885a9831874fb9026b2
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-18 21:39:51 +0530

    importing sum&multiply modules from calculate folder

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("./xyz.js");
 
 const {calculateSum}=require("./calculate/sum.js");
+const {calculateMultiply}=require("./calculate/multiply.js");
 
 console.log(globalThis===global);
 
 var a=10;
 var b=20;
 calculateSum(a,b);
+calculateMultiply(a,b);
