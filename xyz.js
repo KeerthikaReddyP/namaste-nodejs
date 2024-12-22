e6ab9f2ec63669a8b51e3ac9264a170f54126ed8
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-22 19:14:09 +0530

    Explanation to those 5 steps of require execution

--- xyz.js
+++ xyz.js
@@ -2,9 +2,10 @@
 
 
 /*
-    1. Resolving the module
-    2. Loading the module
+    1. Resolving the module (checking whether the path is local, json, or util)(js file or json file or util or folder..)
+    2. Loading the module (file content is loaded according to the file type)
     3. Wrapping it inside an IIFE
-    4. Code Evalueation
-    5. Caching
+    4. Code Evalueation (module.exports)
+    5. Caching (module is cached, so that it won't run all these 5 steps again and again, when required by multiple modules)
+        (It just returns from cache) (Efficient) (doesn't slow down)
 */
