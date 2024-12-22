313b0bd4e4d0211312d8897e34eee89f933d966e
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-22 16:07:37 +0530

    require

--- xyz.js
+++ xyz.js
@@ -8,6 +8,8 @@
 //It is also wrapped inside a function before executing.
 
 (function (module,require,...) {
+    require("./path");
+    // require another module inside xyz module
   function calculateMultiply(a, b) {
     console.log(a * b);
   }
