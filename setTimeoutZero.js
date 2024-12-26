c0a35b029f641bb723384665200e06c5c7ccb584
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-26 21:25:50 +0530

    setTimeout after 0 sec

--- setTimeoutZero.js
+++ setTimeoutZero.js
@@ -3,6 +3,13 @@ var b=8765586;
 
 console.log("Hellooo");
 
+// It wot be called immediately.
+// It will be called as soon as the call stack is empty.(Definitely after more than 0 sec)
+setTimeout(()=>{
+    console.log("Call me RIGHT NOW!!!");
+},0);
+
+
 setTimeout(()=>{
     console.log("Call me after 3 secs");
 },3000);
