b59e73bf0327d9c4df1dd0a3f36c1cc653c5113b
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-26 21:04:10 +0530

    Blocks the main thread

--- blocking.js
+++ blocking.js
@@ -4,7 +4,7 @@ var a=1003;
 var b=32;
 
 // Synchronous (It don't take any callback)
-crypto.pbkdf2Sync("password","salt",500000,50,"sha512");
+crypto.pbkdf2Sync("password","salt",5000000,50,"sha512");
 console.log("Key is generated synchronously by blocking the main thread");
 
 // Password Based Key Derivation Function
