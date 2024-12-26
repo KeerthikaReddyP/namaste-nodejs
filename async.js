5bfe6991c4097b6d2d64cc321ce39f9680a61737
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-26 18:58:07 +0530

    async, libuv

---
+++ async.js
@@ -0,0 +1,27 @@
+const fs=require("fs");
+const https=require("https");
+
+console.log("Hello world");
+
+var a=1029232;
+var b=2342;
+
+https.get("https://dummyjson.com/products/1",(res)=>{
+    console.log("Data fetched successfully");
+});
+
+setTimeout(()=>{
+    console.log("Timeout for 5 secs");
+},5000);
+
+fs.readFile("./file.txt","utf8", (err,data)=>{
+    console.log("File data ", data);
+});
+
+function multiplyFn(x,y){
+    const result=x*y;
+    return result;
+}
+
+var c=multiplyFn(a,b);
+console.log(c);
