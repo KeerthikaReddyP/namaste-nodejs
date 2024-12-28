6773f8cc2f97ca8f2f0dce04b31b017d79818b0e
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-28 16:32:58 +0530

    We can change the size of thread pool.

--- thread-pool.js
+++ thread-pool.js
@@ -1,5 +1,7 @@
 const crypto=require("node:crypto");
 
+process.env.UV_THREADPOOL_SIZE=2;
+
 crypto.pbkdf2("password","salt", 500000,50,"sha512",(err,key)=>{
     console.log("1-crypto pbkdf2 DONE");
 });
