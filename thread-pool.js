77e190cee473475d636ca8ccf602c618902c8c51
Keerthika Reddy <keerthikareddy267@gmail.com>
2024-12-28 16:28:27 +0530

    Thread pool (4 threads)

--- thread-pool.js
+++ thread-pool.js
@@ -8,4 +8,14 @@ crypto.pbkdf2("password","salt", 500000,50,"sha512",(err,key)=>{
     console.log("2-crypto pbkdf2 DONE");
 });
 
+crypto.pbkdf2("password","salt", 500000,50,"sha512",(err,key)=>{
+    console.log("3-crypto pbkdf2 DONE");
+});
+
+crypto.pbkdf2("password","salt", 500000,50,"sha512",(err,key)=>{
+    console.log("4-crypto pbkdf2 DONE");
+});
+
 // They take same time to complete and printed at the same time.
+// Now, these 4 calls also same time and printed at the same time.
+// Because thread pool has 4 thread by default, and each call is attached to each thread.
