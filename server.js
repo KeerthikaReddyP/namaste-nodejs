86acb9ba083d4b3e8134b948d0839321c399473c
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-01 16:26:13 +0530

    Server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http=require("node:http");
 
-const server=http.createServer();
+const server=http.createServer(function(req,res){
+    res.end("Hellooo worldd!!!!!!");
+});
 
 server.listen(7777);
 
