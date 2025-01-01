0e0fe3fb7990e9db7f2fe0f3fed2344e41bb6f70
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-01 16:29:27 +0530

    Server again

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http=require("node:http");
 
 const server=http.createServer(function(req,res){
+    if(req.url ==="/getSecretData") res.end("There's no secret dataa...Hehe");
+    
     res.end("Hellooo worldd!!!!!!");
 });
 
