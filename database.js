c7a17bf6316d6f0e822c5788915b11873b311f81
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-04 20:22:36 +0530

    Database

--- database.js
+++ database.js
@@ -1,1 +1,8 @@
 const URI="mongodb+srv://keerthikareddy267:S8MUnIUbV9cX9V6L@nodebykeer.oxcbl.mongodb.net/";
+
+// Go to mongodb website
+// Create a free cluster
+// Create a user
+// Get the connection string
+// Install mongo db compass
+// Create your database & Add data
