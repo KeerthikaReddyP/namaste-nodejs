05afafa1e984b140410640cd300c791755ccfde9
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-16 22:55:49 +0530

    Connecting to the server

--- database.js
+++ database.js
@@ -1,7 +1,31 @@
-require("mongodb");
+const { MongoClient } = require("mongodb");
 
+//Connection string
 // const URI =
-  // "mongodb+srv://keerthikareddy267:S8MUnIUbV9cX9V6L@nodebykeer.oxcbl.mongodb.net/";
+//   "mongodb+srv://keerthikareddy267:S8MUnIUbV9cX9V6L@nodebykeer.oxcbl.mongodb.net/";
+
+const client = new MongoClient(URI);
+
+//Our DB name
+const dbName = "HelloWorld";
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+
+  return "Done";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+
+
+
 
 // Go to mongodb website
 // Create a free cluster
